7c789767f695db44945792bdcd8140be1c19a827
CodeMaker46 <shikshak013@gmail.com>
2025-04-04 00:45:52 +0530

    backend set up

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const connectDB = require('./config/database');
+// const authRoutes = require('./routes/auth');
+// const userRoutes = require('./routes/user');
+
+const app = express();
+dotenv.config();
+connectDB();
+
+
+// CORS middleware with permissive settings for Instagram
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 600
+}));
+
+
+app.use(express.json());
+
+// Routes 
+// app.use('/api/auth', authRoutes);
+
+module.exports = app;
