c145b98b5bc0d949eb2f28d84dc4b7eb72e90f0d
CodeMaker46 <shikshak013@gmail.com>
2025-04-04 17:31:50 +0530

    login page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './components/pages/LandingPage.jsx'
+import Login from './components/pages/Login.jsx'
+import About from './components/pages/About.jsx'
+import Features from './components/pages/Features.jsx'
+import Pricing from './components/pages/Pricing.jsx'
+import Contact from './components/pages/Contact.jsx'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* <Route path="/about" element={<AboutPage />} />
-      <Route path="/features" element={<FeaturesPage />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="/login" element={<LoginPage />} />   */}
+      <Route path="/about" element={<About/>} />
+      <Route path="/features" element={<Features />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="/login" element={<Login />} />  
+
+      {/* Protected routes */}
+      
     </Routes>
   )
 }
